bb7f786e7aa2d3b816b3f7ce9c7c9142400f0895
dpazmino98 <d.pazmino@vaance.com>
2023-04-28 12:53:41 +0200

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/', [ProductController::class, 'show'])->name("index");
+
+Route::get('/carts', [CartController::class, 'show'])->name("carts");
+
+Route::get('/carts/create', [CartController::class, 'create'])->name("cart.create");
+
+Route::post('/carts/store', [CartController::class, 'store'])->name("cart.store");
+
+Route::get('/carts/delete/{id}', [CartController::class, 'delete'])->name("cart.delete");
+
+Route::get('/carts/detail/{id}', [CartController::class, 'detail'])->name("carts.detail");
+
+Route::get('/carts/edit/{id}', [CartController::class, 'edit'])->name("carts.edit"); 
+
+Route::post('/carts/removeProducts', [CartController::class, 'removeProducts'])->name("cart.removeProducts");
