8a56661bb1e5b8f78bb390a14d6afbf1f35b2985
dpazmino98 <d.pazmino@vaance.com>
2023-04-28 12:49:49 +0200

    softdeletes added

--- database/migrations/2023_04_26_134000_create_products_table.php
+++ database/migrations/2023_04_26_134000_create_products_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('name');
             $table->double('price');
             $table->integer('cart_id')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
--- database/migrations/2023_04_26_134326_create_cart_table.php
+++ database/migrations/2023_04_26_134326_create_cart_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->integer('NProd')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
